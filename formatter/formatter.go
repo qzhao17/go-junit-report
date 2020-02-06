4dfba6defc32399126d266c3dc492a3f2278db9a
QIHAN ZHAO <qzhao@lyft.com>
2020-02-05 21:30:12 -0800

    rename

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/qzhao17/go-junit-report/parser"
 )
 
 // JUnitTestSuites is a collection of JUnit test suites.
@@ -120,7 +120,7 @@ func JUnitReportXML(report *parser.Report, noXMLHeader bool, goVersion string, w
 			if test.Result != parser.PASS && test.Result != parser.FAIL && test.Result != parser.SKIP{
                 ts.Errors++
 			}
-			
+
 			ts.TestCases = append(ts.TestCases, testCase)
 		}
 
--- go-junit-report.go
+++ go-junit-report.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jstemmer/go-junit-report/formatter"
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/qzhao17/go-junit-report/formatter"
+	"github.com/qzhao17/go-junit-report/parser"
 )
 
 var (
