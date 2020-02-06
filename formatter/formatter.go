6b95aabec8ee7b9c53e659860a717eb3b47dde99
QIHAN ZHAO <qzhao@lyft.com>
2020-02-06 11:45:18 -0800

    fix

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -14,7 +14,7 @@ import (
 
 // JUnitTestSuites is a collection of JUnit test suites.
 type JUnitTestSuites struct {
-	XMLName xml.Name         `xml:" "`
+	XMLName xml.Name         
 	Suites  []JUnitTestSuite `xml:"testsuite"`
 }
 
